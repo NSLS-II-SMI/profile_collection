d25432225cc0f55afd80fefb1ef3f7d0c8a3cc6c
Thomas A Caswell <tcaswell@bnl.gov>
2016-06-07 17:44:36 -0400

    Add boiler plate

---
+++ profile_collection/startup/00-startup.py
@@ -0,0 +1,63 @@
+# Make ophyd listen to pyepics.
+from ophyd import setup_ophyd
+setup_ophyd()
+
+# Subscribe metadatastore to documents.
+# If this is removed, data is not saved to metadatastore.
+from bluesky.global_state import gs
+
+# ###### UN-COMMENT THESE LINES TO USE MDS
+# import metadatastore.commands
+# gs.RE.subscribe_lossless('all', metadatastore.commands.insert)
+
+# # At the end of every run, verify that files were saved and
+# # print a confirmation message.
+# from bluesky.callbacks.broker import verify_files_saved
+# gs.RE.subscribe('stop', post_run(verify_files_saved))
+
+# Import matplotlib and put it in interactive mode.
+import matplotlib.pyplot as plt
+plt.ion()
+
+# Make plots update live while scans run.
+from bluesky.utils import install_qt_kicker
+install_qt_kicker()
+
+# Optional: set any metadata that rarely changes.
+# RE.md['beamline_id'] = 'YOUR_BEAMLINE_HERE'
+
+# convenience imports
+from ophyd.commands import *
+from bluesky.callbacks import *
+from bluesky.spec_api import *
+from bluesky.global_state import gs, abort, stop, resume
+from databroker import (DataBroker as db, get_events, get_images,
+                        get_table, get_fields, restream, process)
+from time import sleep
+import numpy as np
+
+RE = gs.RE  # convenience alias
+
+# Uncomment the following lines to turn on verbose messages for debugging.
+# import logging
+# ophyd.logger.setLevel(logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
+
+from functools import partial
+from pyOlog import SimpleOlogClient
+from bluesky.callbacks.olog import logbook_cb_factory
+
+# Set up the logbook. This configures bluesky's summaries of
+# data acquisition (scan type, ID, etc.).
+
+LOGBOOKS = ['Data Acquisition']  # list of logbook names to publish to
+simple_olog_client = SimpleOlogClient()
+generic_logbook_func = simple_olog_client.log
+configured_logbook_func = partial(generic_logbook_func, logbooks=LOGBOOKS)
+
+cb = logbook_cb_factory(configured_logbook_func)
+RE.subscribe('start', cb)
+
+# This is for ophyd.commands.get_logbook, which simply looks for
+# a variable called 'logbook' in the global IPython namespace.
+logbook = simple_olog_client
