f5901c742fe6e2d9d69b7cc5a4e8c745815b8368
Maksim Rakitin <mrakitin@bnl.gov>
2018-06-29 15:16:13 -0400

    Implemented initial version of the PyDM screens as SMI
    Based on the .ui files from:
    - https://slaclab.github.io/pydm-tutorial/action/designer_inline.html
    - https://slaclab.github.io/pydm-tutorial/action/designer_expert.html
    
    Conda env was created as follows:
    - conda create -p /opt/conda_envs/pydm python=3.6 -y
    - conda install -c https://conda.anaconda.org/pydm-tag -c https://conda.anaconda.org/conda-forge pydm -y
    
    Usage:
    ~/.ipython/profile_collection/startup/pydm/pydm.sh

---
+++ profile_collection/startup/pydm/pydm.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+. /opt/conda/etc/profile.d/conda.sh
+conda activate pydm
+
+for motor in '{"MOTOR": "XF:12IDC-ES:2{WAXS:1-Ax:Arc}Mtr"}' \
+             '{"MOTOR": "XF:12IDC-OP:2{HEX:PRS-Ax:Rot}Mtr"}'; do
+    echo "Starting $motor..."
+    pydm --hide-nav-bar -m "$motor" inline_motor.ui > /dev/null 2>&1 &
+done
+
