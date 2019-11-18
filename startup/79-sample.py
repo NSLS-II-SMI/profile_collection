3ec0ff677041ef5b1fc84ca8ad8aab9af664840b
gfreychet <gfreychet@gmail.com>
2019-11-18 11:07:45 -0500

    Move the Pilatus definition to the Pilatus file

--- startup/79-sample.py
+++ startup/79-sample.py
@@ -14,21 +14,6 @@ waxs_pressure = Waxs_chamber_pressure('XF:12IDC-VA:2', name='waxs_chamber_pressu
 waxs_pressure.ch1_read.kind = 'hinted'
 
 
-class PIL1MPositions(Device):
-    x = Cpt(EpicsMotor, 'X}Mtr')
-    y = Cpt(EpicsMotor, 'Y}Mtr')
-    z = Cpt(EpicsMotor, 'Z}Mtr')
-
-
-class PIL1MBS(Device):
-    x = Cpt(EpicsMotor, 'IBB}Mtr')
-    y = Cpt(EpicsMotor, 'IBM}Mtr')
-    x_other = Cpt(EpicsMotor, 'OBB}Mtr')
-    y_other = Cpt(EpicsMotor, 'OBM}Mtr')
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.x_center = None
-        self.y_center = 13.1
 
 
 att1_1 = TwoButtonShutter('XF:12IDC-OP:2{Fltr:1-1}', name='att1_1')
@@ -58,8 +43,8 @@ att2_11 = TwoButtonShutter('XF:12IDC-OP:2{Fltr:2-11}', name='att2_11')
 att2_12 = TwoButtonShutter('XF:12IDC-OP:2{Fltr:2-12}', name='att2_12')
 
 
-pil1m_pos = PIL1MPositions('XF:12IDC-ES:2{Det:1M-Ax:', name='pil1m_pos')
-pil1m_bs = PIL1MBS('XF:12IDC-ES:2{BS:SAXS-Ax:', name='pil1m_bs')
+
+
 
 for detpos in [pil1m_pos]:
     detpos.configuration_attrs = detpos.read_attrs
