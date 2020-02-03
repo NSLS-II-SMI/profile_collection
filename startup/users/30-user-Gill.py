8b7114c8522e6ec34348ef2aebb6631a14059d52
Mikhail Zhernenkov <zherne@bnl.gov>
2020-02-03 08:40:45 -0500

    Last version of user-Gill

--- startup/users/30-user-Gill.py
+++ startup/users/30-user-Gill.py
@@ -1,19 +1,24 @@
 
-def gill_giwaxs(exp_time=0.3):
-    dets = [pil300KW, pil1M]
-    name = 'Salt_nickel_sapphire_cool_300-0.3s'  
-    inc_angle = [0.12, 0.3, 0.4]
+def gill_giwaxs(exp_time=0.5):
+    dets = [pil300KW]
+    name = 'Ni_111_Salt-RTcoolx2_0.5s'  
+    #initial_angle_zero = -0.72
+    inc_angle = [0.12, 0.22, 0.3]
+    phi = [0]
     waxs_arc = [0, 39, 7]
-    #yield from alignement_gisaxs_hex(angle = 0.1)
-    for incident_angle in inc_angle:
-        name_fmt = '{sample}_{angle}deg'
-        yield from bps.mvr(stage.th, incident_angle)
-        sample_name = name_fmt.format(sample=name, angle=incident_angle)
-        print(f'\n\t=== Sample: {sample_name} ===\n')
-        sample_id(user_name='SG', sample_name=sample_name)
-        det_exposure_time(exp_time, exp_time)
-        yield from bp.scan(dets, waxs, *waxs_arc)
-        yield from bps.mvr(stage.th, -incident_angle)
+    #yield from bps.mv(stage.th, initial_angle_zero)
+    for ph in phi:
+        yield from bps.mv(prs, ph)
+        yield from alignement_gisaxs_hex_short(angle = 0.08)
+        for incident_angle in inc_angle:
+            name_fmt = '{sample}_phi{phiii}deg_{angle}deg'
+            yield from bps.mvr(stage.th, incident_angle)
+            sample_name = name_fmt.format(sample=name, phiii=ph, angle=incident_angle)
+            print(f'\n\t=== Sample: {sample_name} ===\n')
+            sample_id(user_name='SG', sample_name=sample_name)
+            det_exposure_time(exp_time, exp_time)
+            yield from bp.scan(dets, waxs, *waxs_arc)
+            yield from bps.mvr(stage.th, -incident_angle)
     
     sample_id(user_name='test', sample_name='test')
     det_exposure_time(0.5, 0.5)
