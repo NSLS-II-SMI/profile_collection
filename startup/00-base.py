0abe2a4504ae76e9e5a7c78d334d190e38d3dd42
Mikhail Zhernenkov <zherne@bnl.gov>
2020-03-17 15:46:57 -0400

    Update the metadata required as base at SMI

--- startup/00-base.py
+++ startup/00-base.py
@@ -2,14 +2,16 @@ print(f'Loading {__file__}')
 
 import warnings
 import nslsii
+from ophyd import Signal
+
 nslsii.configure_base(get_ipython().user_ns, 'smi')
 # nslsii.configure_olog(get_ipython().user_ns, subscribe=False)
 
-
-from ophyd import Signal
-beamline_name = Signal(name='beamline_name', value = 'SMI')
-
-#Need here a way of checking if a value is there. If not ask for a new value
+'''
+Metadata: Will be read from the last bluesky documents if bluesky crashed
+This is the base of metadatawhich would be never/rarely changed : facility and beamline names
+Cycle, proposal number, main proposer and comments
+'''
 
 try:
     cycle = db[-1].table(stream_name='baseline')['cycle'][1]
@@ -24,15 +26,16 @@ except:
     main_proposer = ''
     optinal_comments = ''
 
- 
+beamline_name = Signal(name='beamline_name', value = 'SMI') 
 cycle = Signal(name='cycle', value = cycle)
 proposal_number = Signal(name='proposal_number', value = proposal_number)
 main_proposer = Signal(name='main_proposer', value = main_proposer)
 optinal_comments = Signal(name='optinal_comments', value = optinal_comments)
 
-base_md = {'beamline_name' : 'SMI',
-           'cycle': cycle.value,
-           'proposal_number': proposal_number.value,
-           'main_proposer': main_proposer.value,
-           'optinal_comments': optinal_comments.value
-           }
+base_md = { 'facility' : 'NSLS-II',
+            'beamline_name' : 'SMI',
+            'cycle': cycle.value,
+            'proposal_number': proposal_number.value,
+            'main_proposer': main_proposer.value,
+            'optinal_comments': optinal_comments.value
+            }
