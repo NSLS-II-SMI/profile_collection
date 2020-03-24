1615c2ae4d8cb1dd067995dae71769bb74604743
gfreychet <gfreychet@gmail.com>
2020-03-24 10:08:17 -0400

    Update GISAXS scan + creation of a basic one

--- startup/40-scans.py
+++ startup/40-scans.py
@@ -1,22 +1,22 @@
+from ophyd import Signal
 
 def expert_gisaxs_scan(dets = [pil300KW, pil1M],
                         sam_name=['test'], 
-                        motors = [energy],
-                        motor_range=[[16000, 16100, 16200]],
-                        waxs_arc = [[0, 13, 3]],
+                        motors = [[waxs_arc], [piezo.th]],
+                        motor_range=[[0, 13, 3], [0.1, 0.2, 3]],
                         measurement_time = 1,
                         number_images = 1,
                         user_name = 'IC',
-                        alphai = 0.15,
-                        realignement = False,
-                        x_trans = None,
-                        scan_type = 'Default',
+                        realignement = [False],
+                        x_trans = [0],
+                        scan_type = 'linspace', #list
                         md = None):
 
 
     #Check if what is planned is doable
+    motor_names = [motor for moto in motors for motor in moto]
     try:
-        [motor for motor in motors]
+        motor_names
         [det for det in dets]
     except:
         raise Exception('Motors or detectors not known')
@@ -54,31 +54,37 @@ def expert_gisaxs_scan(dets = [pil300KW, pil1M],
 
     #Update metadata for motors not used and add the motor as detector if so
     # ToDo: what other values do we need to track, lakeshore and ...
-    if 'piezo' in [motor.name for motor in motors]: all_detectors.append(piezo)
+    if 'piezo' in [motor_names]: all_detectors.append(piezo)
     else: sd.baseline.append(piezo)
 
-    if 'stage' in [motor.name for motor in motors]: all_detectors.append(stage)
+    if 'stage' in [motor_names]: all_detectors.append(stage)
     else: sd.baseline.append(stage)
 
-    if 'prs' in [motor.name for motor in motors]: all_detectors.append(prs)
+    if 'prs' in [motor_names]: all_detectors.append(prs)
     else: sd.baseline.append(prs)
    
-    if 'energy' in [motor.name for motor in motors]: all_detectors.append(energy)
+    if 'energy' in [motor_names]: all_detectors.append(energy)
     else: sd.baseline.append(energy)
 
 
     #Control if the input values are correct or if anything missing ...
-    # ToDO: list the options of possible scans and figure out all the assumptions
-    # ToDO: Can we write something general enough: or do we need
+    # ToDo: How to handle all the other parameters: check if it is a list, or len!=1 if so assertions needed plus assigned values
+    #  Realignement and xtrans
 
-    assert(len(motors) == len(motor_range), f'Number of motor range ({len(motor_range)}) is different from number of motors ({len(motors)})')
-    assert(len(sam_name) == len(motor_range), f'Number of sam_name ({len(sam_name)}) is different from number of motor range ({len(motor_range)})')   
-    assert(scan_type in ['default_scan', 'list_scan', 'grid_scan', 'spiral_scan'], f'Scan ({scan_type}) not defined. Viable options: default_scan, list_scan, grid_scan, spiral_scan')
+    assert(len(motor_names) == len(motor_range), f'Number of motor range ({len(motor_range)}) is different from number of motors ({len(motor_names)})')
+    motor_dic = {}
+    for (motor, motor_ran) in zip(motors, motor_range):
+        assert (len(motor_ran) == 3, f'Motor range ({motor_ran}) expect 3 values but only ({len(motor_ran)}) given')
+        motor_dic = motor_dic + {motor.name: {motor_ran[0], motor_ran[1], motor_ran[2]}}
 
-    if len(motors) != 1:
-        #This need to be discussed
-        raise Exception('Too many motors defined for GISAXS/GIWAXS')
-
+    if len(sam_name) != 1 and '.x' not in motor_names:
+        raise Exception('There should be only one sample name since x is not scanned')
+    elif len(sam_name) == 1 and '.x' in motor_names:
+        motor_name = [motor for motor in motor_names if '.x' in motor][0]
+        sam_name = [[sam_name]] * list(motor_dic[motor_name])[2]
+    elif len(sam_name) != 1 and '.x' in motor_names:
+        motor_name = [motor for motor in motor_names if '.x' in motor][0]
+        assert (len(sam_name) == list(motor_dic[motor_name])[2], f'Number of sample({len(sam_name)}) is different from the ({list(motor_dic[motor_name])[2]}) x position given')
 
     #Create signal for exposure_time, sample_name
     #Record motor position as detector if moved and as baseline if not
@@ -102,6 +108,111 @@ def expert_gisaxs_scan(dets = [pil300KW, pil1M],
     
     sample_id(user_name='test', sample_name='test')
     det_exposure_time(0.5,0.5)
+
+
+
+
+
+def gisaxs_scan(dets=[pil300KW, pil1M],
+                sam_name=['test'],
+                motors=[[waxs_arc], [piezo.th]],
+                motor_range=[[0, 13, 3], [0.1, 0.2, 3]],
+                measurement_time=1,
+                number_images=1,
+                user_name='GF',
+                realignement=[False],
+                x_trans = [0],
+                scan_type = 'linspace',
+                md = None
+                ):
+
+
+    # Check if what is planned is doable
+    motor_names = [motor for moto in motors for motor in moto]
+    print('motor names', motor_names)
+    try:
+        motor_names
+        [det for det in dets]
+    except:
+        raise Exception('Motors or detectors not known')
+
+    print('Motor and detectors existing')
+
+    # If this values change over a scan, possibility to transform in ophyd signal and record over a scan
+    base_md = {'plan_name': 'gi_scan',
+               'detectors': [det.name for det in dets],
+               'user_name': user_name,
+               'motor_scanned': [motor.name for motor in motors],
+               'exposure_time': measurement_time,
+               'number_image': number_images,
+               'realignement': realignement,
+               'x_trans': x_trans,
+               }
+
+    base_md.update(md or {})
+
+    print('base_md set')
+
+
+    all_detectors = dets  # Start a list off all the detector to trigger
+    all_detectors.append([xbpm2, xbpm3, ring.current])  # add all the values to track  for analysis
+    sd.baseline = []  # Initializatin of the baseline
+    SMI.get_md()  # update metadata from the beamline
+
+    # Update metadata for the detectors
+    if 'pil300KW' in [det.name for det in dets]:
+        all_detectors.append(WAXS)  # Record the position of the WAXS arc and beamstop
+        sd.baseline.append(smi_waxs_detector)  # Load metadata of WAXS detector in the baseline
+
+    if 'pil1M' in [det.name for det in dets]:
+        SMI_SAXS_Det()  # Update metadata from the beamline
+        # Nothing added as detector so far since the saxs detector is not moved but anything could be implemented
+        sd.baseline.append(smi_saxs_detector)  # Load metadata of SAXS detector in the baseline
+
+    if 'rayonix' in [det.name for det in dets]:
+        print('no metadata for the rayonix yet')
+
+    print('update detctor in baseline and detctor list')
+
+    # Update metadata for motors not used and add the motor as detector if so
+    # ToDo: what other values do we need to track, lakeshore and ...
+    if 'piezo' in [motor_names]:
+        all_detectors.append(piezo)
+    else:
+        sd.baseline.append(piezo)
+
+    if 'stage' in [motor_names]:
+        all_detectors.append(stage)
+    else:
+        sd.baseline.append(stage)
+
+    if 'prs' in [motor_names]:
+        all_detectors.append(prs)
+    else:
+        sd.baseline.append(prs)
+
+    if 'energy' in [motor_names]:
+        all_detectors.append(energy)
+    else:
+        sd.baseline.append(energy)
+
+    print('update motors in baseline and detctor list')
+
+
+    sample_na = Signal(name='sample_name', value = sam_name)
+    all_detectors.append(sample_na)
+
+    det_exposure_time(measurement_time, number_images * measurement_time)
+    print('opening the run')
+
+    yield from bps.open_run(md=base_md)
+    yield from bp.scan(all_detectors, motor_names, *motor_range)
+    yield from close_run()
+
+    print('GISAXS scan with metadata done')
+
+
+
 
 
 
