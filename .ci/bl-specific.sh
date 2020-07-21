70c8ae957ce889be308025ab07598a97cd9e1dd6
Maksim Rakitin <mrakitin@bnl.gov>
2020-07-21 13:47:26 -0400

    Create a directory in advance

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cp -v smi_config.csv ~/.ipython/profile_${TEST_PROFILE}/smi_config.csv
+
+mkdir ~/.ipython/profile_${TEST_PROFILE}/startup/
 cp -v startup/intepolation_db_sdd2.txt ~/.ipython/profile_${TEST_PROFILE}/startup/intepolation_db_sdd2.txt
