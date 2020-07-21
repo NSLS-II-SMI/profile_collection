9a6f68d92ec06e43b51628158df338212fd0fbea
Maksim Rakitin <mrakitin@bnl.gov>
2020-07-21 13:21:45 -0400

    Copy to startup/

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp -v smi_config.csv ~/.ipython/profile_${TEST_PROFILE}/smi_config.csv
-cp -v startup/intepolation_db_sdd2.txt ~/.ipython/profile_${TEST_PROFILE}/intepolation_db_sdd2.txt
+cp -v startup/intepolation_db_sdd2.txt ~/.ipython/profile_${TEST_PROFILE}/startup/intepolation_db_sdd2.txt
