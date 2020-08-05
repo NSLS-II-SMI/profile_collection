8675c3b1172ba7fb6e5075eae46b08a714c655d9
Mikhail Zhernenkov <zherne@bnl.gov>
2020-08-05 08:47:20 -0400

    User macros update 2020_2

--- startup/users/30-user-UCR.py
+++ startup/users/30-user-UCR.py
@@ -36,3 +36,31 @@ def run_mesh_fastUCR(t=0.5):
     det_exposure_time(0.3,0.3)
 
 
+def run_mesh_fastUCI(t=1): 
+    waxs_range = [0, 6.5, 13, 19.5, 26]
+    name = 'TW'
+    dets = [pil300KW]
+    det_exposure_time(t,t)
+    
+    
+    samples = ['tooth4_bot', 'tooth4_cen-tip', 'Mag-gypsum', 'Mag-cell', 'Mag-nocell', 'tooth0pm', 'tooth1pm', 'tooth2pm', 'tooth3pm', 'tooth4pm', 'tooth5pm']
+    x_list = [    17045,          17190,           3220,       -12740,      -32520,      27950,     28550,       29000,     29450,       29850,      30250]
+    y_list = [    -4110,          -4160,            670,        -1800,       -1160,      -1650,     -1500,       -1650,     -1750,      -1500,       -1000]
+    x_range=[ [0, 125, 6],  [0, 400, 17],    [0, 200, 9],  [0, 200, 9], [0, 200, 9], [0, 250, 6], [0, 300, 7],[0, 300, 7], [0, 350, 8], [0, 400, 9], [0, 350, 8]]
+    y_range=[[0, 200, 101],[0, 200, 101],  [0, 200, 101],[0, 200, 101],[0, 200, 101],[0, 350, 36],[0, 400, 41],[0, 400, 41],[0, 400, 41],[0, 450, 46],[0, 500, 51]]
+    
+
+    assert len(x_list) == len(samples), f'Number of X coordinates ({len(x_list)}) is different from number of samples ({len(samples)})'
+    for x, y, sample, x_r, y_r in zip(x_list, y_list, samples, x_range, y_range):
+        yield from bps.mv(piezo.x, x)
+        yield from bps.mv(piezo.y, y)
+        for wa in waxs_range:
+            yield from bps.mv(waxs, wa)
+            name_fmt = '{sam}_wa{waxs}'
+            sample_name = name_fmt.format(sam=sample, waxs='%2.1f'%wa)
+            sample_id(user_name=name, sample_name=sample_name) 
+            print(f'\n\t=== Sample: {sample_name} ===\n')
+            yield from bp.rel_grid_scan(dets, piezo.y, *y_r, piezo.x, *x_r, 0)
+
+    sample_id(user_name='test', sample_name='test')
+    det_exposure_time(0.3,0.3)
