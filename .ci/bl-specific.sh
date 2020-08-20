8c3237318980f4ed41c1394dd5bbe1a69836bf8d
Maksim Rakitin <mrakitin@bnl.gov>
2020-08-20 00:38:39 -0400

    Use collection-2020-2.0rc7-1 branch of profile-collection-ci and remove BL-specific copying step

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -2,5 +2,3 @@
 
 cp -v smi_config.csv ~/.ipython/profile_${TEST_PROFILE}/smi_config.csv
 
-mkdir ~/.ipython/profile_${TEST_PROFILE}/startup/
-cp -v startup/intepolation_db_sdd2.txt ~/.ipython/profile_${TEST_PROFILE}/startup/intepolation_db_sdd2.txt
