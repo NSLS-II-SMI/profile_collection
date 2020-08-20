57aa16f293566c6b211d6b56cda721c1d0ec7af6
Maksim Rakitin <mrakitin@bnl.gov>
2020-08-20 12:20:51 -0400

    Implement PersistentDict in 00-base.py (a workaround for old conda envs 2019-3.0 and older)

--- startup/00-base.py
+++ startup/00-base.py
@@ -14,9 +14,52 @@ logger = logging.getLogger('ophyd')
 logger.setLevel('WARNING')
 
 #from bluesky.utils import PersistentDict
+from pathlib import Path
 
-#Try to set this new path
-#RE.md = PersistentDict('~/profile_collection/startup/')
+import appdirs
+
+
+try:
+    from bluesky.utils import PersistentDict
+except ImportError:
+    import msgpack
+    import msgpack_numpy
+    import zict
+
+    class PersistentDict(zict.Func):
+        def __init__(self, directory):
+            self._directory = directory
+            self._file = zict.File(directory)
+            super().__init__(self._dump, self._load, self._file)
+
+        @property
+        def directory(self):
+            return self._directory
+
+        def __repr__(self):
+            return f"<{self.__class__.__name__} {dict(self)!r}>"
+
+        @staticmethod
+        def _dump(obj):
+            "Encode as msgpack using numpy-aware encoder."
+            # See https://github.com/msgpack/msgpack-python#string-and-binary-type
+            # for more on use_bin_type.
+            return msgpack.packb(
+                obj,
+                default=msgpack_numpy.encode,
+                use_bin_type=True)
+
+        @staticmethod
+        def _load(file):
+            return msgpack.unpackb(
+                file,
+                object_hook=msgpack_numpy.decode,
+                raw=False)
+
+runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
+
+# PersistentDict will create the directory if it does not exist
+RE.md = PersistentDict(runengine_metadata_dir)
 
 RE.md['beamline_name'] = 'SMI'
 RE.md['facility'] = 'NSLS-II'
