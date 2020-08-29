1eea45a3b886b2c0daeec715ce94f27da24d0ba3
Maksim Rakitin <mrakitin@bnl.gov>
2020-08-29 17:58:52 -0400

    HACK: retry to put to amptek's trigger button twice if the acquisition has not sttarted

--- startup/20-amptek.py
+++ startup/20-amptek.py
@@ -1,5 +1,6 @@
 print(f'Loading {__file__}')
 
+import time as ttime
 import numpy as np
 from ophyd.status import SubscriptionStatus
 from ophyd.mca import EpicsMCA
@@ -95,6 +96,7 @@ class AmptekSoftTrigger(BlueskyInterface):
                                "Call the stage() method before triggering.")
 
         def callback(value, old_value, **kwargs):
+            print(f'  {self.name} old value {old_value} --> new_value {value}')
             if int(round(old_value)) == 1 and int(round(value)) == 0:
                 if self._starting or self._starting is None:
                     self.starting = False
@@ -104,7 +106,24 @@ class AmptekSoftTrigger(BlueskyInterface):
                 return False
 
         status = SubscriptionStatus(self.mca.when_acq_stops, callback, run=False)
-        self._acquisition_signal.set(1)
+        print(f'  !!! attempting to put to {self._acquisition_signal.pvname} value 1')
+        # ttime.sleep(0.1)
+        timeout = 10  # s
+        self._acquisition_signal.put(1)
+
+        print(f'  !!! sleeping 0.1s after putting')
+        t0 = ttime.time()
+        print(f'Start waiting at {ttime.ctime(t0)}...')
+        while True:
+            ttime.sleep(0.01)
+            if int(round(self.mca.when_acq_stops.get())) == 1:
+                print(f'Success, {self.mca.when_acq_stops.pvname} was set to 1. Waited for {ttime.time() - t0}s.')
+                break
+            else:
+                if ttime.time() - t0 > timeout:
+                    print(f'Waited for {timeout}s, but the signal {self.mca.when_acq_stops.pvname} did not change. Attempting again.')
+                    self._acquisition_signal.put(1)
+                    break
 
         return status
 
