1353c349d98da611417f56de93c11c80bd6638e5
Mikhail Zhernenkov <zherne@bnl.gov>
2020-09-24 10:26:25 -0400

    Add a default amptek scan

--- startup/users/30-user-Greer.py
+++ startup/users/30-user-Greer.py
@@ -43,3 +43,38 @@ def mapping_saxs_Greer(t=5):
         
     sample_id(user_name='test', sample_name='test')
     det_exposure_time(0.3,0.3)
+
+
+def mapping_saxs_test(t=0.1): 
+
+    samples = ['test']
+    name = 'test'
+    x_list = [0]
+    y_list = [0]
+    
+    x_range=[[0, 0, 2]]
+    y_range=[[0, 0, 1]]
+    
+    # Detectors, motors:
+    dets = [pil1M, pil300KW, amptek]# dets = [pil1M,pil300KW]
+    det_exposure_time(t,t)
+
+    assert len(x_list) == len(samples), f'Number of X coordinates ({len(x_list)}) is different from number of samples ({len(samples)})'
+    assert len(x_list) == len(y_list), f'Number of X coordinates ({len(x_list)}) is different from Y coordinates ({len(y_list)})'
+    assert len(x_list) == len(x_range), f'Number of X coordinates ({len(x_list)}) is different X ranges ({len(x_range)})'
+    assert len(x_list) == len(y_range), f'Number of X coordinates ({len(x_list)}) is different Y ranges ({len(y_range)})'
+
+
+    
+    for x, y, sample, x_r, y_r in zip(x_list, y_list, samples, x_range, y_range):
+        yield from bps.mvr(piezo.x, x)
+        yield from bps.mvr(piezo.y, y)
+        name_fmt = '{sam}'
+        sample_name = name_fmt.format(sam=sample)
+        sample_id(user_name=name, sample_name=sample_name) 
+        print(f'\n\t=== Sample: {sample_name} ===\n')
+            
+        yield from bp.rel_grid_scan(dets, piezo.x, *x_r, piezo.y, *y_r,  0) #1 = snake, 0 = not-snake
+        
+    sample_id(user_name='test', sample_name='test')
+    det_exposure_time(0.3,0.3)
