b86b9bc269720e708ed25496d815640ba38f6c3f
Guillaume Freychet <gfreychet@gmail.com>
2020-12-14 18:35:28 -0500

    Change the file number og WAXS because needde when loaded the shutter file. Added a function to convert string return from pressure to real pressure

---
+++ startup/07-waxschamber.py
@@ -0,0 +1,32 @@
+from ophyd import ( Component as Cpt,  Device,
+                    EpicsSignal, EpicsSignalRO, EpicsMotor)
+
+print(f'Loading {__file__}')
+
+# Read the pressure from the waxs chamber
+class sample_chamber_pressure(Device):
+    waxs = Cpt(EpicsSignal, '{Det:300KW-TCG:7}P:Raw-I')  # Change PVs
+    maxs = Cpt(EpicsSignal, '{B1:WAXS-TCG:9}P:Raw-I')  # Change PVs
+
+
+def get_chamber_pressure(signal):
+    value = signal.get()
+    try:
+        print(float(value))
+        return float(value)
+    except:
+        if isinstance(value, str) and value.startswith('LO'):
+            print(np.float('1E-03'))
+            return np.float('1E-03')
+        raise
+
+
+chamber_pressure = sample_chamber_pressure('XF:12IDC-VA:2', name='chamber_pressure')  # Change PVs
+chamber_pressure.waxs.kind = 'hinted'
+chamber_pressure.maxs.kind = 'hinted'
+
+
+GV7 = TwoButtonShutter('XF:12IDC-VA:2{Det:1M-GV:7}', name='GV7')
+
+
+
