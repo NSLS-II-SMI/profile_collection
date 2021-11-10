cf2b82e302d0f002ba00880bb57f1ba2b89443f5
gfreychet <gfreychet@gmail.com>
2021-11-10 09:54:31 -0500

    Change calculation of harmonic with energy

--- startup/11-energy.py
+++ startup/11-energy.py
@@ -19,22 +19,36 @@ D_Si111 = 3.1293
 
 
 # Converters:
+# def energy_to_gap(target_energy, undulator_harmonic=1):
+#     fundamental_energy = target_energy / float(undulator_harmonic)
+#     f = fundamental_energy
+#     #measured fundamnetal harmonic on the 20th September 2018
+#     a = 24.31415 #-53.025391 values until 20Sep'18
+#     b1 = -0.08224 #0.220837
+#     b2 = 1.38266e-4 #-3.537803e-4
+#     b3 = -1.18903e-7 #-13.105219e-7
+#     b4 = 5.90784e-11 #-1.587795e-10
+#     b5 = -1.70272e-14 #4.734179e-14
+#     b6 = 2.64385e-18 #-7.633003e-18
+#     b7 = -1.70455e-22 #5.14881e-22
+#     gap_mm = a + b1*f + b2*f**2 + b3 * f**3 + b4 * f**4 + b5 * f**5 + b6 * f**6 + b7 * f**7
+#     gap = gap_mm*1000 - 33 #-30 for 12.62; -33 for 14 keV; -21 for 16.1 keV; -50 for 9540eV; -20 for 2450eV; -45 for 4050eV
+#     #-50 for 9700 keV -- 11.150 keV, -55 for 6.55 keV, -35 for 7.7 keV
+#     return gap
+
+
+
 def energy_to_gap(target_energy, undulator_harmonic=1):
     fundamental_energy = target_energy / float(undulator_harmonic)
     f = fundamental_energy
-    #measured fundamnetal harmonic on the 20th September 2018
-    a = 24.31415 #-53.025391 values until 20Sep'18
-    b1 = -0.08224 #0.220837
-    b2 = 1.38266e-4 #-3.537803e-4
-    b3 = -1.18903e-7 #-13.105219e-7
-    b4 = 5.90784e-11 #-1.587795e-10
-    b5 = -1.70272e-14 #4.734179e-14
-    b6 = 2.64385e-18 #-7.633003e-18
-    b7 = -1.70455e-22 #5.14881e-22
-    gap_mm = a + b1*f + b2*f**2 + b3 * f**3 + b4 * f**4 + b5 * f**5 + b6 * f**6 + b7 * f**7
-    gap = gap_mm*1000 - 21 #-30 for 12.62; -33 for 14 keV; -21 for 16.1 keV; -50 for 9540eV; -20 for 2450eV; -45 for 4050eV
-    #-50 for 9700 keV -- 11.150 keV, -55 for 6.55 keV, -35 for 7.7 keV
+
+    gap_mm = -533.56314 + (1926.52257) * (0.28544/(1+10**((-10782.55855-f)*1.44995e-4))+(1-0.28544)/(1+10**((7180.06758-f)*6.34167e-4)))
+    # gap_mm = a + b1*f + b2*f**2 + b3 * f**3 + b4 * f**4 + b5 * f**5 + b6 * f**6 + b7 * f**7
+    gap = gap_mm*1000 - 21#-30 for 12.62; -33 for 14 keV; -21 for 16.1 keV, 18.25keV; -50 for 9540eV; -20 for 2450eV; -45 for 4050eV
+    #-50 for 9700 keV -- 11.150 keV, -55 for 6.55 keV, -35 for 7.7 kev
+    
     return gap
+
 
 
 def energy_to_bragg(target_energy, delta_bragg=0):
@@ -112,14 +126,14 @@ class Energy(PseudoPositioner):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._hints = None
-        try:
-            previous_harmonic = db[-1].table(stream_name='baseline')['energy_harmonic'][1]
-        except Exception:
-            warnings.warn(f'Previous databroker record was not read. '
-                          f'Setting harmonic to the default value {self.target_harmonic.get()}')
-            previous_harmonic = self.target_harmonic.get()
+        # try:
+        #     previous_harmonic = db[-1].table(stream_name='baseline')['energy_harmonic'][1]
+        # except Exception:
+        #     warnings.warn(f'Previous databroker record was not read. '
+        #                   f'Setting harmonic to the default value {self.target_harmonic.get()}')
+        #     previous_harmonic = self.target_harmonic.get()
 
-        self.target_harmonic.put(previous_harmonic)
+        # self.target_harmonic.put(previous_harmonic)
 
     @pseudo_position_argument
     def forward(self, p_pos):
@@ -144,6 +158,7 @@ class Energy(PseudoPositioner):
         if energy <= 2050:
             raise ValueError("The energy you entered is too low ({} eV). "
                              "Minimum energy = 2050 eV".format(energy))
+
         if energy >= 24001:
             raise ValueError('The energy you entered is too high ({} eV). '
                              'Maximum energy = 24000 eV'.format(energy))
