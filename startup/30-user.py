52ce71eb74cfd81a0c86e1e1afbd6a4f616bacbc
gfreychet <gfreychet@gmail.com>
2021-12-06 13:36:53 -0500

    Clean the table extraction info saving

--- startup/30-user.py
+++ startup/30-user.py
@@ -1,12 +1,12 @@
 print(f'Loading {__file__}')
 
-def export_scan( sid, filename='', path='/home/xf12id/tmp/',verbose=True ):
+def export_scan( sid, filename='', path='/home/xf12id/users/',verbose=True ):
     """
     Export table by giving a scan id
     """
     hdr = db[sid]
     d = hdr.table()
-    output = path + 'sid=%s_%s.csv'%(sid,filename)
+    output = path + 'sid=%s.csv'%(filename)
     d.to_csv( output )
     if verbose:
         print( 'The table of sid=%s is saved as %s.'%(sid, output) )
