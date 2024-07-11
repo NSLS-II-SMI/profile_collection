aeda36cd66821c993b5188a3ebe0bcd2d728d273
SMI operator <noemail@bnl.gov>
2024-07-11 10:00:03 -0400

    Change energy ivu gap LUT

--- startup/11-energy.py
+++ startup/11-energy.py
@@ -36,7 +36,7 @@ def energy_to_gap(target_energy, undulator_harmonic=1, man_offset=0):
         + (1 - 0.28544) / (1 + 10 ** ((7180.06758 - f) * 6.34167e-4))
     )
     e_exp = np.array([ 2450, 2470, 3600, 4050, 6510, 6550, 7700, 8980, 9700, 12000, 12620, 14000, 14400, 16100,])
-    off_exp = np.array([-20,  -35,   29,   30,   -2,    55,   35,   19,   20,    15,    30,     3,     38,    20,])
+    off_exp = np.array([-20,  -35,   29,   30,   -2,    55,   35,   19,   50,    45,    45,     45,     45,    45,])
 
 
     auto_offset = np.interp(target_energy, e_exp, off_exp, left=min(off_exp), right=max(off_exp))
@@ -46,8 +46,8 @@ def energy_to_gap(target_energy, undulator_harmonic=1, man_offset=0):
     if target_energy < 6000:
         #K edge -10
         #Cl edge -20
-
         gap = (gap_mm * 1000 -20)
+        #gap = (gap_mm * 1000 -48)
 
     # -30 for 12.62; -33 for 14 keV; -21 for 16.1 keV, 18.25keV; -50 for 9540eV; -20 for 2450eV; -30 for 4050eV
     # -29 for 9700 keV -- 11.150 keV, -55 for 6.55 keV, -35 for 7.7 kev;  -29 for 3.6 keV
@@ -194,6 +194,8 @@ class Energy(PseudoPositioner):
     def set(self, position):
         (energy,) = position
         # print(position, self.position)
+
+        # TODO change self.settle_time here based on energy we are moving to
         if np.abs(energy - self.position[0]) < 0.01:
             return MoveStatus(self, energy, success=True, done=True)
         return super().set([float(_) for _ in position])
